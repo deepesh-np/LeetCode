53469c0969d0c5062a7e368e2996cf429a4d7e40
Deepesh_np <nrndpsh@gmail.com>
2026-01-05 20:03:43 +0530

    LC-min-stack [Runtime: 5 ms; Memory: 46.9 MB]

--- design/min-stack.java
+++ design/min-stack.java
@@ -1,40 +1,31 @@
 class MinStack {
-    int min = Integer.MAX_VALUE;
-    ArrayList<Integer> list;
-    Stack <Integer> s;
+    Stack<Integer> s;
+    Stack<Integer> minStack;
+
     public MinStack() {
-        s = new Stack <> ();
-        list = new ArrayList<>();
+        s = new Stack<>();
+        minStack = new Stack<>();
     }
-    
+
     public void push(int val) {
         s.push(val);
-        min = Math.min(min, val);
-        list.add(min);
-    }
-    
-    public void pop() {
-        int p = s.pop();
-        if (list.get(list.size() - 1) == p){
-            list.remove((list.size() - 1));
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
         }
-
     }
-    
+
+    public void pop() {
+        s.pop();
+        minStack.pop();
+    }
+
     public int top() {
         return s.peek();
     }
-    
+
     public int getMin() {
-        return list.get(list.size() - 1);
+        return minStack.peek();
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
